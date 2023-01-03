454b43067002381e4a9ee7cbbccc57274a6339ed
Igor Canalli <igorcanalli@gmail.com>
2023-01-02 21:13:21 -0300

    Obtendo um usuário por id

--- src/main/java/com/canalli/workshopmongo/services/UserService.java
+++ src/main/java/com/canalli/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.canalli.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.canalli.workshopmongo.domain.User;
 import com.canalli.workshopmongo.repository.UserRepository;
+import com.canalli.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,14 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repositorio.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> user = repositorio.findById(id);
+		if(!user.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}
+		return user.get();
 	}
 
 }
---
+++ src/main/java/com/canalli/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.canalli.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
